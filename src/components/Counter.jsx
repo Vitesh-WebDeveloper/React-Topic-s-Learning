8eb67ec8afe19de856364eacaa3859ec017c84af
Pulluri Sai Vitesh <saivitesh4@gmail.com>
2026-08-25 19:56:34 +0530

    Learned React Hooks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -15,33 +15,45 @@ const Counter = () => {
 // }
 
 
-const [count,SetTime] = useState(0);
-let timesetter = useRef();
-    const start = () =>  {
-      timesetter.current = setInterval(() => {
-            SetTime(count => count + 1);
-        }, 1000);
-      }
-    const stop = () =>  {
-        clearInterval(timesetter.current);
-        timesetter.current = null;
-      }
-    const reset = () =>  {
-        stop();
-        SetTime(0);
-      }
+// const [count,SetTime] = useState(0);
+// let timesetter = useRef();
+//     const start = () =>  {
+//       timesetter.current = setInterval(() => {
+//             SetTime(count => count + 1);
+//         }, 1000);
+//       }
+//     const stop = () =>  {
+//         clearInterval(timesetter.current);
+//         timesetter.current = null;
+//       }
+//     const reset = () =>  {
+//         stop();
+//         SetTime(0);
+//       }
 
+// let a = useRef(0);
+// function changeColor() {
+//   if (a.current.style.color === "black") {
+//     a.current.style.color = "blue"
+//   }
+//   else {
+//     a.current.style.color = "black"
+//   }
+// }
 
-let a = useRef(0);
   return (
 
     <div>
       {/* <p>The current Count is {count}</p>
       <button onClick={handlefn}>ClickMe</button> */}
-      <h1>{count}</h1>
-      <button onClick={start}>Start</button>
+      {/* <h1>{count}</h1>
+      <button ref = {a} onClick={start}>Start</button>
       <button onClick={stop}>Stop</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={reset}>Reset</button> */}
+{/* 
+      <h1 ref = {a}>hollllaaa</h1>
+      <button onClick={changeColor}>clickme</button> */}
+      
     </div>
   )
 
